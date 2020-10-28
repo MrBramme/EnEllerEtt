93f661dd992bb46cfe84526b551c34a1668b9951
Bram Hendrickx <bram_hendrickx@hotmail.com>
2020-10-28 20:58:37 +0100

    added first test for the wikiparser

--- Bram.EnEllerEtt.Adapter.AngleSharp.UnitTests/WiktionaryParserUnitTests.cs
+++ Bram.EnEllerEtt.Adapter.AngleSharp.UnitTests/WiktionaryParserUnitTests.cs
@@ -1,5 +1,10 @@
-﻿using Bram.EnEllerEtt.Core.Interface.Adapters;
+﻿using Bram.EnEllerEtt.Adapter.AngleSharp.UnitTests.Resources;
+using Bram.EnEllerEtt.Core.Interface.Adapters;
+using Bram.EnEllerEtt.Core.Interface.Models;
+using FluentAssertions;
 using NUnit.Framework;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Bram.EnEllerEtt.Adapter.AngleSharp.UnitTests
 {
@@ -11,6 +16,30 @@ namespace Bram.EnEllerEtt.Adapter.AngleSharp.UnitTests
         public void Setup()
         {
             _sut = new WiktionaryParser();
+        }
+
+        [Test]
+        public async Task ParseFromHtmlAsync_GivenBarnHtmlString_ReturnsCorrectResult()
+        {
+            // Given
+            var expected = new WordResult
+            {
+                WordType = WordType.Ett,
+                PluralGenitivBestämd = "barnens",
+                PluralGenitivObestämd = "barns",
+                PluralNominativBestämd = "barnen",
+                PluralNominativObestämd = "barn",
+                SingleGenitivBestämd = "barnets",
+                SingleGenitivObestämd = "barns",
+                SingleNominativBestämd = "barnet",
+                SingleNominativObestämd = "barn",
+            };
+
+            // When
+            var result = await _sut.ParseFromHtmlAsync(HtmlStrings.Barn, CancellationToken.None);
+
+            // Then
+            result.Should().BeEquivalentTo(expected);
         }
     }
 }
